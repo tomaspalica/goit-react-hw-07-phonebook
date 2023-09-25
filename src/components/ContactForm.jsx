a22a6c0d9b0e6eb975c862c344a44c3dd8ef8666
tomaspalica <tomasss.palica@gmail.com>
2023-09-25 10:19:20 +0200

    refactoring

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,30 +1,28 @@
-import { useEffect, useState } from 'react';
 import css from '../css/ContactForm.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { addContact } from 'redux/operations';
+import { selectContacts } from 'redux/selectors';
 const ContactForm = () => {
-  const contactsList = useSelector(state => state.contacts.items);
-  const state = useSelector(state => state);
-  const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const contactsList = useSelector(selectContacts);
 
-  const handleChange = e => {
-    if (e.currentTarget.name === 'name') {
-      setName(e.currentTarget.value);
-    } else {
-      setNumber(e.currentTarget.value);
-    }
-  };
+  const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements[0].value;
+    const number = form.elements[1].value;
+
     if (contactsList.find(el => el.name === name)) {
       alert(`${name} is already in contacts`);
     } else {
-      console.log(state.contacts);
-      dispatch(addContact({ name, number }));
+      dispatch(
+        addContact({
+          name,
+          number,
+        })
+      );
     }
   };
 
@@ -33,25 +31,21 @@ const ContactForm = () => {
       <label className={css.label}>
         name
         <input
-          value={name}
           type="text"
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          onChange={handleChange}
         />{' '}
       </label>
       <label className={css.label}>
         Number
         <input
-          value={number}
           type="tel"
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          onChange={handleChange}
         />
       </label>
       <button>add contact</button>
--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,13 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 import css from '../css/ContactsList.module.css';
-
 import { useEffect } from 'react';
 import { fetchcontacts, deleteContacts } from 'redux/operations';
+import { selectFilterNames } from 'redux/selectors';
 export const ContactsList = () => {
-  const filter = useSelector(state => state.filter);
-  const contacts = useSelector(state => state.contacts.items);
   const dispatch = useDispatch();
-  console.log(contacts);
+
   useEffect(() => {
     dispatch(fetchcontacts());
   }, [dispatch]);
@@ -17,27 +15,11 @@ export const ContactsList = () => {
     dispatch(deleteContacts(el.target.id));
   };
 
-  const filterNames = () => {
-    if (filter === '') {
-      return contacts;
-    }
-    const filterValue = filter.toLowerCase();
-    const filteredUsers = contacts.filter(({ name }) => {
-      const nameValue = name.toLowerCase();
-      return nameValue.includes(filterValue);
-    });
-    return filteredUsers;
-  };
+  const filteredNames = useSelector(selectFilterNames);
 
-  // const normalizedFilter = filter.toLowerCase();
-
-  // const filteredContacts = contacts.filter(({ name }) => {
-  //   name.toLowerCase().includes(normalizedFilter);
-  // });
-  console.log(filterNames());
   return (
     <ul className={css.nameList}>
-      {filterNames().map(({ id, name, number }) => (
+      {filteredNames.map(({ id, name, number }) => (
         <li key={id}>
           {name}: {number}{' '}
           <button id={id} name={name} onClick={contactDelete}>
